4b41e5613aaec9a8fae1a298f02927f7b064bf48
Abdulbasit <aabdulrafiu721@gmail.com>
2023-04-08 09:49:47 +0000

    Function finds the balance factor of a tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,13 +11,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height = 0, right_height = 0;
 	int balance = 0;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 
 	if (tree->left)
 		left_height = binary_tree_balance(tree->left) + 1;
 	if (tree->right)
 		right_height = binary_tree_balance(tree->right) + 1;
+
+	if (!tree->left && !tree->right)
+		return (0);
 
 	balance = left_height - right_height;
 	return (balance);
